a38a15681174115376b866b572dbab543c023541
Timothy Wu <tw6658@g.rit.edu>
2022-08-24 23:03:48 -0400

    PE1

---
+++ Wu_PE1/Wu_PE1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Timothy Wu";
+            int x = 1;
+            int y = 2;
+           
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(name);
+            Console.WriteLine(x + y);
+            Console.WriteLine(y - x);
+
+        }   
+    }
+}
