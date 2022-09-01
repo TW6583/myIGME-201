2758906c39fbaa50c1e2d33e072a0f6f19da9e37
Timothy Wu <tw6658@g.rit.edu>
2022-08-31 23:51:01 -0400

    PE03

---
+++ PE03/PE03/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
+
+
+/*
+Question 1
+
+namespace fabulous
+  {
+    super.smashing.great;
+  }
+  
+ namespace super
+  {
+       namespace smashing
+       {
+            string smashing = " ";
+       }
+  
+  }
+
+*/
+
+/*
+Question 2
+
+Illegal variable names:
+99Flake
+iLike#Tags
+$bankBalance
+Factorial!
+black&white
+
+*/
+
+/*
+Quesiton 3
+
+No it is not too large to fit in a string variable because there is no limit to naming a variable
+
+*/
+
+/*
+Question 4
+
+The first steps involving the expression is * and /, then %, then +, and lastly +=.
+
+*/
+
+//Question 5
+/*int? value1 = null;
+int? value2 = null;
+int? value3 = null;
+int? value4 = null;
+
+
+
+//prompts user to input a number in
+System.Console.Write("Enter a number: \n");
+
+//Reads the user input and stores it in a variable
+value1 = Convert.ToInt32(System.Console.ReadLine());
+
+//Reads the user input and stores it in a variable
+value2 = Convert.ToInt32(System.Console.ReadLine());
+
+//Reads the user input and stores it in a variable
+value3 = Convert.ToInt32(System.Console.ReadLine());
+
+//Reads the user input and stores it in a variable
+value4 = Convert.ToInt32(System.Console.ReadLine());
+
+Console.WriteLine(value1, value2, value3, value4);
+
+
+*/
+/*
+Question 6
+
+Compiler: A program that translates source code into machine code so the CPU can understand it
+Identifier: the name of a primitive data type
+Primitive data type: Single value data items
+Class: A template that describes the properties and behaviors of an object
+Constant: A variable that does not change.
+Parsing: A process of turning data into another form of data
+
+*/
+
+/*
+Question 7
+
+a) 4
+b) 3
+c) - 10
+d) 17
+e) 2.5
+
+*/
+
+/*
+Question 8
+
+num1 = 10
+num2 = 2
+num3 = 1
+
+*/
+
+/*
+Question 9
+
+a) int amountOfMoney is a string rather than an integer
+b) I would fix it by removing the " " because when the number is in quotes, it is turned into a string rather than an integer.
+
+*/
+
+/*
+Question 10
+
+Choice a is not smartly named because it is all lowercased and the name does not show much information about the purpose of the variable.
+
+*/
+
+/*
+Question 11
+a) Logical error
+b) Run-time error
+c) Compile-time error
+d) Compile-time error
+e) Logical error
+f) Compile-time error
+
+*/
+
+/*
+Quesiton 12
+
+Hi thereDavid
+
+*/
+
+/*
+Question 13
+
+50 plus 25 is 5025
+
+*/
+
+/*
+Question 14
+
+System.Console.Write("Please type your name below. \n");
+Console.Write("Your name is" + " " + System.Console.ReadLine() + "!\n");
+
+*/
+
+/*
+Question 15
+
+string myName = Timothy;
+
+*/
+
+/*
+Question 16
+
+myName = myName + " " + "Wu";
+
+*/
+
+/*
+Question 17
+
+C# does not see these as equivalent because they both have different bit values.
+
+*/
+
+/*
+Question 18
+
+Convert.Double(input);
+Console.WriteLine(input);
+
+*/
+
+/*
+Question 19
+
+b. int userNumber = int.Parse(userInput);
+
+*/
+
+/*
+Question 20
+
+1) F
+2) F
+3) T
+4) F
+
+*/
