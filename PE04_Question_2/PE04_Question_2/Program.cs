8490841deb63b455fbaebec46f9fd350de9c1427
Timothy Wu <tw6658@g.rit.edu>
2022-09-04 00:36:35 -0400

    PE04 Q2

---
+++ PE04_Question_2/PE04_Question_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE04_Question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double userNum1, userNum2;
+
+            Console.WriteLine("Enter a number:");
+            userNum1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter another number:");
+            userNum2 = Convert.ToDouble(Console.ReadLine());
+
+            if (userNum1 > 10 && userNum2 > 10)
+            {
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine(userNum1);
+                Console.WriteLine(userNum2);
+            }
+        }
+    }
+}
