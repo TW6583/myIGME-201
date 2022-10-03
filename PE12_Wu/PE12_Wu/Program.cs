e4d23c7b8330fe36d8e779d3b7f5d5f71f6d5aa1
Timothy Wu <tw6658@g.rit.edu>
2022-10-02 22:48:44 -0400

    PE11 Q3

---
+++ PE12_Wu/PE12_Wu/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE12_Wu
+{
+    internal class Program
+    {
+        public class MyClass
+        {
+            private string MyString;
+            public string myString
+            {
+                set { myString = MyString; }
+            }
+            public string returnedString
+            {
+                get { return MyString; }
+            }
+            public virtual string GetString()
+            {
+                return MyString;
+            }
+        }
+
+        public class MyDerivedClass : MyClass
+        {
+            public override string GetString()
+            {
+                return base.GetString() + "(output from the derived class)";    
+            }
+
+            
+        }
+
+        static void Main(string[] args)
+        {
+            MyDerivedClass myDerivedClass = new MyDerivedClass();
+            Console.WriteLine(myDerivedClass.returnedString);
+        }
+    }
+}
