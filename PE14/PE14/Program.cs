f58aa91969bef82ed17bfbf7d26d809c32952681
Timothy Wu <tw6658@g.rit.edu>
2022-10-09 14:42:33 -0400

    PE14 Q1

---
+++ PE14/PE14/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE14
+
+    //Name: Timothy Wu
+    //PE14
+
+{
+    //PE14 Q1
+    //public abstract class that reads and writes a private string
+    public abstract class readWriteClass
+    {
+        private string privString;
+
+        public string stringAccessor
+        {
+            get
+            {
+                return privString;
+            }
+            set
+            {
+                privString = value;
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
