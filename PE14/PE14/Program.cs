e3f68544678d04875c2b5b2301e5cbeda0a184b2
Timothy Wu <tw6658@g.rit.edu>
2022-10-09 15:04:04 -0400

    PE14 Q2

--- PE14/PE14/Program.cs
+++ PE14/PE14/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -28,6 +29,39 @@ namespace PE14
             }
         }
     }
+
+
+    //PE 14 Q2
+    //Interface that will be inherited by two classes
+    public interface anInterface
+    {
+        string strInterface();
+    }
+
+    //class that inherits anInterface and will add two ints and will return the sum along with a string stating its number
+    public class addingNums : anInterface
+    {
+        public int nOne;
+        public int nTwo;
+
+        public string strInterface()
+        {
+            return this.nOne + this.nTwo + ": is the sum";
+        }
+    }
+
+    //class that inherits anInterface and combines two strings
+    public class combineStrings : anInterface
+    {
+        public string strOne;
+        public string strTwo;
+
+        public string strInterface()
+        {
+            return this.strOne + " " + this.strTwo;
+        }
+    }
+
 
 
     internal class Program
