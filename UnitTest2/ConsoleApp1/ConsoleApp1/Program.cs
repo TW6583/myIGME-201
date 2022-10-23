555c494cfe8fa45c57a760774d3cb31eb578acfa
Timothy Wu <tw6658@g.rit.edu>
2022-10-23 16:53:46 -0400

    Question 4

---
+++ UnitTest2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    //Questions 4
+
+    public abstract class Phone
+    {
+        private string phoneNumber;
+        public string address;
+
+        public string PhoneNumber;
+        public abstract void Connect();
+        public abstract void Disconnect();
+
+    }
+
+    public interface PhoneInterface
+    {
+        void Answer();
+        void MakeCall();
+        void HangUp();
+
+    }
+
+    public class RotaryPhone : Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+
+        public override void Connect()
+        {
+
+        }
+
+        public override void Disconnect()
+        {
+
+        }
+    }
+
+    public class PushBottonPhone : Phone, PhoneInterface
+    {
+        public void Answer()
+        {
+
+        }
+        public void MakeCall()
+        {
+
+        }
+
+        public void HangUp()
+        {
+
+        }
+
+        public override void Connect()
+        {
+
+        }
+
+        public override void Disconnect()
+        {
+
+        }
+
+    }
+
+    public class Tardis : RotaryPhone
+    {
+        private bool sonicScrewdriver;
+        private byte whichDrWho;
+        private string femaleSideKick;
+        public double exteriorSurfaceArea;
+        public double interiorVolume;
+
+        public byte WhichDrWho
+        {
+            get
+            {
+                return this.whichDrWho;
+            }
+        }
+
+        public string FemaleSideKick
+        {
+            get
+            {
+                return this.femaleSideKick;
+            }
+        }
+
+        public void TimeTravel()
+        {
+
+        }
+
+       
+    }
+
+    public class PhoneBooth : PushBottonPhone
+    {
+        private bool superMan;
+        public double costPerCall;
+        public bool phoneBook;
+
+        public void OpenDoor()
+        {
+
+        }
+
+        public void CloseDoor()
+        {
+
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
