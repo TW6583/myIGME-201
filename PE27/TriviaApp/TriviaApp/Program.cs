66f18680379ed26afacf08023fc99b3adcf6248e
Timothy Wu <tw6658@g.rit.edu>
2022-11-23 22:36:32 -0500

    PE27

---
+++ PE27/TriviaApp/TriviaApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Web;
+using static System.Net.WebRequestMethods;
+
+
+namespace TriviaApp
+{
+    class TriviaResult
+    {
+        public string category;
+        public string type;
+        public string difficulty;
+        public string question;
+        public string correct_answer;
+        public List<string> incorrect_answers   ;
+    }
+
+    class Trivia
+    {
+        public int response_code;
+        public List<TriviaResult> results;
+
+    }
+    //{"response_code":0,
+    //"results":[{"category":"Science: Computers","type":"multiple","difficulty":"medium",
+    //"question":"Which coding language was the #1 programming language in terms of usage on GitHub in 2015?",
+    //"correct_answer":"JavaScript","incorrect_answers":["C#","Python","PHP"]
+    //}]}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = null;
+            string s = null;
+
+            HttpWebRequest request;
+            HttpWebResponse response;
+            StreamReader reader;
+
+            url = "https://opentdb.com/api.php?amount=1&type=multiple";
+
+            request = (HttpWebRequest)WebRequest.Create(url);
+            response = (HttpWebResponse)request.GetResponse();
+            reader = new StreamReader(response.GetResponseStream());
+            s = reader.ReadToEnd();
+            reader.Close();
+
+            Trivia trivia = JsonConvert.DeserializeObject<Trivia>(s);
+
+            trivia.results[0].question = HttpUtility.HtmlDecode(trivia.results[0].question);
+            trivia.results[0].correct_answer = HttpUtility.HtmlDecode(trivia.results[0].correct_answer);
+
+            for (int i = 0; i < trivia.results[0].incorrect_answers.Count; ++i)
+            {
+                trivia.results[0].incorrect_answers[i] = HttpUtility.HtmlDecode(trivia.results[0].incorrect_answers[i]);
+            }
+
+        }
+    }
+}
