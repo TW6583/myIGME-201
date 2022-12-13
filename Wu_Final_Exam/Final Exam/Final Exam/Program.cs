8c1243fb595817966b2c2a1ca5da3951117a152d
Timothy Wu <tw6658@g.rit.edu>
2022-12-13 15:02:30 -0500

    Stack made with List

---
+++ Wu_Final_Exam/Final Exam/Final Exam/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Exam
+{
+    internal class Program
+    {
+    public class Stack
+        {
+            public List<int> pseudoStack = new List<int>();
+
+            public void Push(int n)
+            {
+                pseudoStack.Add(n);
+                Console.WriteLine("Pushed: " + n);
+            }
+
+            public void Pop()
+            {
+                pseudoStack.Remove(pseudoStack.Count);
+                Console.WriteLine("Popped");
+            }
+
+            public void Peek()
+            {
+                Console.WriteLine(pseudoStack[pseudoStack.Count - 1]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            stack.Pop();
+
+            stack.Peek();
+
+        }
+    }
+}
---
+++ Wu_Final_Exam/Stack_Class/Stack_Class/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Class
+{
+    public class Stack
+    {
+        public List<int> pseudoStack;
+
+        public void Push(int n)
+        {
+            pseudoStack.Add(n);
+        }
+
+        public void Pop()
+        {
+            pseudoStack.Remove(pseudoStack.Count);
+        }
+
+        public void Peek()
+        {
+            Console.WriteLine(pseudoStack.IndexOf(pseudoStack.Count));
+        }
+    }
+}
